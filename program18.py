9ba3b24b406a7f47d3b4640a5186a26d2b06b9a8
21bca20 <96615091+21bca20@users.noreply.github.com>
2022-03-24 11:29:12 +0530

    program uploaded
    This program has error

---
+++ program18.py
@@ -0,0 +1,28 @@
+#write a script to enter five string in a list and check and print string whose length has even number of charcter.
+#without any string function
+l=[]
+count=0
+def createlist():
+    for i in range(5):
+        s=input("Enter any string")
+        l.append(s)
+def evennumber(l):
+    count=0
+    for i in l:
+        count+=l
+    if len(l)%2==0:
+            print(l)
+    return(count)
+createlist()
+ans=count(even(1))
+print(ans)
+
+
+
+    
+    
+    
+    
+    
+    
+s
